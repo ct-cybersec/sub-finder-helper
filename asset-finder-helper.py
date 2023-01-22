9a57800d952f91667aa2db19a4c0d3d5429020c6
ct-cybersec <80648240+ct-cybersec@users.noreply.github.com>
2023-01-22 12:49:26 -0500

    Finished the Colors; Finished the Progress Bar

--- asset-finder-helper.py
+++ asset-finder-helper.py
@@ -6,17 +6,19 @@ import click
 from tqdm import tqdm
 from time import sleep
 
-# Exception Handling for First Argument
+# Exception Handling for First Argument, Which Needs To Be A URL
 try:
     url = sys.argv[1]
 except:
-    os.system("echo '\033[1mYou forgot the \"url\" argument... Please run the program like the example below: \033[0m'")
-    os.system("echo 'python assetfinder.py example.com'")
+    os.system("echo '\033[1;38;2;255;0;0mYou forgot the URL argument... Please run the program like the example below: \033[0m\npython assetfinder.py ct-cybersec.com'")
+    sys.exit(1)
+
+# Make Sure To Use The Correct URL Formatting For URL Argument. This If Statement Also Disallows A Second Argument. Will Add Special Characters Later
+if (url.startswith(".")) | (url.endswith(".")):
+    os.system("echo '\033[1;38;2;255;0;0mPlease use the correct scheme for a URL (i.e. ct-cybersec.com)\033[0m'")
     sys.exit(1)
 
 # ARRRRTTTTTT
-width = os.get_terminal_size().columns
-
 print("""
 \033[1;38;2;250;250;110m    _   ___ ___ ___ _____   ___ ___ _  _ ___  ___ ___  \033[0m
 \033[1;38;2;170;228;121m   /_\ / __/ __| __|_   _| | __|_ _| \| |   \| __| _ \ \033[0m
@@ -26,81 +28,73 @@ print("""
 \033[1;38;2;23;104;119m            | __ | _|| |__|  _/ _||   /_|              \033[0m
 \033[1;38;2;42;72;88m            |_||_|___|____|_| |___|_|_(_)              
 \033[0m
-\033[1mA Tool To Help Automate AssetFinder, Created By TomNomNom ➡️  (github.com/tomnomnom)
+\033[1m          A Tool To Help Automate AssetFinder\n   [Created By TomNomNom ➡️  (github.com/tomnomnom)]\033[0m
 """)
 sleep(1)
 
-#Code For Execution
-for char in tqdm(["a", "b", "c", "d", "e", "f", "g", "h"], desc="Overall Progress", position=0, colour="green", dynamic_ncols=True):
-    while char == "a":
-        sleep(0.75)
-        tqdm.write("\nStarting The Process For " + url.upper() + "!")
-        sleep(1)
-        char += char
-    while char == "b":
-        sleep(0.25)
-        tqdm.write("1️⃣  Verifying The \"URL\" Folder Exists")
-        sleep(0.25)
+# This Is The Status Bar - Updates After Iteration Through The List. The Sleeps Aren't Really Necessary, But It Makes It Nicer-Looking
+for char in tqdm(range(1, 101, 1), colour = "green", position = 1, leave = False, mininterval = 0.01, smoothing = 1, desc = "Overall Progress: ", bar_format = "{desc:}{percentage:.0f}%|{bar}{r_bar}"):
+    if char == 1:
+        tqdm.write("\n\033[1;38;2;250;250;110mStarting The Process For " + url.upper() + "!\033[0m")
+    if char <= 14:
+        sleep(0.01)
+    if char == 15:
+        tqdm.write("\033[1;38;2;196;236;116m(1) Verifying The \"URL\" Folder Exists\033[0m")
+        # Creating A Directory That Is Labelled As The Url You, The User, Has Put As The Argument
         if not os.path.exists(url):
             os.mkdir(url)
-        tqdm.write("Finished Verification, Moving On")
-        sleep(0.25)
-        char += char
-    while char == "c":
-        tqdm.write("2️⃣  Verifying The \"Recon\" Folder Exists")
-        sleep(0.25)
+        tqdm.write("\033[1;38;2;196;236;116mFinished Verification, Moving On\033[0m")
+    if char <= 30:
+        sleep(0.01)
+    if char == 31:
+        tqdm.write("\033[1;38;2;146;220;126m(2) Verifying The \"Recon\" Folder Exists\033[0m")
         if not os.path.exists(url + "/recon"):
             os.mkdir(url + "/recon")
-        tqdm.write("Finished Verification, Moving On")
-        sleep(0.25)
-        char += char
-    while char == "d":
-        tqdm.write("3️⃣  Creating A File Named \"Assets.txt\"")
-        sleep(0.25)
+        tqdm.write("\033[1;38;2;146;220;126mFinished Verification, Moving On\033[0m")
+    if char <= 45:
+        sleep(0.01)
+    if char == 46:
+        tqdm.write("\033[1;38;2;100;201;135m(3) Creating A File Named \"Assets.txt\"\033[0m")
         open(url + "/recon/assets.txt", "w+")
-        tqdm.write("Finished Creating File, Moving On")
-        sleep(0.25)
-        char += char
-    while char == "e":
-        tqdm.write("4️⃣  Running Assetfinder By TomNomNom For " + url.upper() + " & Placing Results Into Assets.txt")
-        sleep(0.25)
+        tqdm.write("\033[1;38;2;100;201;135mFinished Creating File, Moving On\033[0m")
+    if char <= 60:
+        sleep(0.01)
+    if char == 61:
+        tqdm.write("\033[1;38;2;57;180;142m(4) Running Assetfinder By TomNomNom For " + url.upper() + " & Placing Results Into \"Assets.txt\"\033[0m")
         os.system('assetfinder ' + url + ' >> ' + url + '/recon/assets.txt')
-        tqdm.write("Finished Running Assetfinder, Moving On")
-        sleep(0.25)
-        char += char
-    while char == "f":
-        tqdm.write("5️⃣  Opening Assets.txt File For Reading & Transferring Results From Assetfinder To Final.txt File")
-        sleep(0.25)
-        with open(url + '/recon/assets.txt') as f:
+        tqdm.write("\033[1;38;2;57;180;142mFinished Running Assetfinder, Moving On\033[0m")
+    if char <= 75:
+        sleep(0.01)
+    if char == 76:
+        tqdm.write("\033[1;38;2;8;159;143m(5) Opening \"Assets.txt\" File For Reading\033[0m")
+        with open(url + '/recon/assets.txt', 'r') as f:
             lines = f.readlines()
-        tqdm.write("Finished Opening Assets.txt For Reading, Moving On")
-        sleep(0.25)
-        char += char
-    while char == "g":
-        tqdm.write("6️⃣  Transferring Results From Assets.txt To Final.txt (i.e. Getting Rid of Duplicates & Any Result Without " + url.upper() + ")")
-        sleep(0.25)
+        lines_set = set(lines)
+        tqdm.write("\033[1;38;2;8;159;143mFinished Opening \"Assets.txt\" For Reading, Moving On\033[0m")
+    if char <= 90:
+        sleep(0.01)
+    if char == 91:
+        tqdm.write("\033[1;38;2;0;137;138m(6) Transferring Results From \"Assets.txt\" To \"Final.txt\" (i.e. Getting Rid of Duplicates & Any Result Without " + url.upper() + ")\033[0m")
         with open(url + '/recon/final.txt', 'w') as f:
-            for line in lines:
-                if url in line:
-                    f.write(line)
-        tqdm.write("Finished Transferring Results, Moving On")
-        sleep(0.25)
-        char += char
-    while char == "h":
-        sleep(0.75)
-        tqdm.write("7️⃣  Cleaning Everything Up")
-        sleep(0.5)
-        tqdm.write("Finished Running The Program!")
-        sleep(0.75)
-        char += char
-print("\nQuestion [In Dwight Schrute's Voice]... Would You Like To Delete The Assets.txt File & Just Keep The Final.txt File? Blank = Yes")
+            for new_line in lines_set:
+                f.write(new_line)
+        tqdm.write("\033[1;38;2;0;137;138mFinished Transferring Results, Moving On\033[0m")
+    if char <= 99:
+        sleep(0.01)
+    if char == 100:
+        tqdm.write("\033[1;38;2;8;115;127m(7) Cleaning Everything Up\033[0m")
+        sleep(0.02)
+        tqdm.write("\033[1;38;2;8;115;127mFinished Running The Program!\033[0m")
+        sleep(0.01)
+    if char == 100:
+        sleep(1)
+tqdm.write("\nQuestion [In Dwight Schrute's Voice]... Would You Like To Delete The \"Assets.txt\" File & Just Keep The \"Final.txt\" File? Blank = Yes\033[0m")
 if (click.confirm("", default=True)):
     os.remove(url + '/recon/assets.txt')
-    os.system("echo '\nRemoved Assets.txt!'")
-    sleep(0.25)
-    os.system("echo 'Done! Buh-Bye 👋'")
-    sleep(0.25)
+    os.system("echo '\nRemoved Assets.txt!\033[0m'")
+    sleep(0.1)
+    os.system("echo 'Done! Buh-Bye 👋\033[0m'")
+    sleep(0.1)
 else:
-    sleep(0.25)
-    os.system("echo '\nDone! Buh-Bye 👋'")
-    sleep(0.25)
+    os.system("echo '\nDone! Buh-Bye 👋\033[0m'")
+    sleep(0.1)
